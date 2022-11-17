ef39bef8ad2acc248da71cfdef2d5eda51c510bc
Igor Gorini <mgigor@hotmail.com>
2022-11-16 21:34:45 -0300

    Deletendo a pasta build e ajustando o .gitignore

--- src/BD/ConnectionFactory.java
+++ src/BD/ConnectionFactory.java
@@ -6,8 +6,8 @@ import java.sql.DriverManager;
 
 public class ConnectionFactory {
     private static final String USERNAME = "root";
-    private static final String SENHA = "senha";
-    private static final String URL = "jdbc:mysql://172.17.0.3:3306/poo?zeroDateTimeBehavior=convertToNull";
+    private static final String SENHA = "";
+    private static final String URL = "jdbc:mysql://localhost:3306/poo?zeroDateTimeBehavior=convertToNull";
     
     public static Connection criarConexao() throws Exception{
         Connection conn = DriverManager.getConnection(URL, USERNAME, SENHA);
